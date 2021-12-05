86ec8582c4d27a1134f7c4e4ebd72cc78ae061d6
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-05 12:15:26 -0500

    Update testbot.py

--- bot/testbot.py
+++ bot/testbot.py
@@ -12,18 +12,18 @@ inter_client = InteractionClient(bot)
 
 @bot.event
 async def on_ready(): 
-	aliveChannel = bot.get_channel(910039460383698995)
+	aliveChannel = bot.get_channel(898150985397063700)
 	await aliveChannel.send('Bot restarted - ' + str(datetime.now())[:-10])
 	await asyncio.sleep(60)
 	if not stayAlive.is_running():
-		stayAlive.start()
+		stayAlive.start(aliveChannel)
 	
 @tasks.loop(seconds=3600) #repeats every 1 hour
-async def stayAlive():
-	aliveChannel, speedChannel = bot.get_channel(898150985397063700), bot.get_channel(915229249974181928)
+async def stayAlive(aliveChannel):
+	speedChannel = bot.get_channel(915229249974181928)
 	wifi  = speedtest.Speedtest()
-	await aliveChannel.send('Bot is alive - ' + str(datetime.now())[:-10])
-	await speedChannel.send("Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nUpload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
+	await aliveChannel.send('Test Bot is alive - ' + str(datetime.now())[:-10])
+	await speedChannel.send("Heroku Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nHeroku Upload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
     
 	
 bot.run(TOKEN, bot=True, reconnect=True)
