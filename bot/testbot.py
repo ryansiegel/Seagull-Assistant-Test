4c27464819c564f5262afb30bf2d75bac50292df
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-05 11:51:00 -0500

    Update testbot.py

--- bot/testbot.py
+++ bot/testbot.py
@@ -1,8 +1,7 @@
-import discord,asyncio,os
+import discord,asyncio,os,speedtest,re
 from datetime import datetime, timedelta, date
 from discord.ext import commands, tasks
 from urllib import parse, request
-import re
 from importlib import reload
 from dislash import *
 
@@ -16,11 +15,15 @@ async def on_ready():
     aliveChannel = bot.get_channel(910039460383698995)
     await aliveChannel.send('Bot restarted - ' + str(datetime.now())[:-10])
     await asyncio.sleep(60)
-    stayAlive.start()
+    if not stayAlive.is_running():
+	stayAlive.start()
 	
 @tasks.loop(seconds=3600) #repeats every 1 hour
 async def stayAlive():
     aliveChannel = bot.get_channel(910039460383698995)
+    speedChannel = bot.get_channel(915229249974181928)
+    wifi  = speedtest.Speedtest()
     await aliveChannel.send('Bot is alive - ' + str(datetime.now())[:-10])
+    await speedChannel.send("Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nUpload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
 	
 bot.run(TOKEN, bot=True, reconnect=True)
