5ea3cef026829cf658f006225dfa36bee457de3a
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-06 13:37:28 -0500

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -1,13 +1,30 @@
-import discord,asyncio,os
-from datetime import datetime, timedelta, date
-from discord.ext import commands, tasks
-from urllib import parse, request
-import re
-from discord.utils import get
-import random
-import string
-import pymysql
-from time import sleep
-import requests
-
-bot = commands.Bot(command_prefix='!', description="This is a Helper Bot")
+async def facts(inter, channelPrint, facts):
+    if facts == "Seagull":
+        fact1 = 'Seagulls are very clever. They learn, remember and even pass on behaviours, such as stamping their feet in a group to imitate rainfall and trick earthworms to come to the surface.'
+        fact2 = 'Seagulls are attentive and caring parents. The male and female pair for life and they take turns incubating the eggs, and feeding and protecting the chicks.'
+        fact3 = 'A small claw halfway up their lower leg enables them to sit and roost on high ledges without being blown off.'
+        fact4 = 'In Native American symbolism, the seagull represents a carefree attitude, versatility, and freedom.'
+        fact5 = 'Seagulls can be found all over the planet including the margins of Antarctica, and are found in the high Arctic, as well.'
+        fact6 = 'Lifespan of seagulls depends on the species. Most seagulls have a lifespan from 10 to 15 years in the wild.'
+        fact7 = 'The smallest seagull species is the little gull (Hydrocoloeus minutus or Larus minutus), with a length of 25–30 cm (9.8–11.8 in), a wingspan of 61–78 cm (24–31 in), and a mass of 68–162 g (2.4–5.7 oz).'
+        fact8 = 'Seagulls are one of the rare animals that are able to drink salt water. They have special glands (located above the eyes) which eliminate excess salt from the body.'
+        fact9 = 'Seagulls often steal food from other birds, animals and people. They occasionally eat young members of their own species (phenomenon called cannibalism).'
+        fact10 = 'Seagulls are monogamous creatures (they mate for a lifetime). Mating couple gathers each year during the mating season to reproduce and to take care of their offspring.'
+        factsList = [fact1, fact2, fact3, fact4, fact5, fact6, fact7, fact8, fact9, fact10]
+        randomFact = random.choice(factsList)
+        embedVar = discord.Embed(title="Seagull Facts!", description=randomFact, color=0x000000)
+        await inter.reply(embed=embedVar)
+        await channelPrint.send('Seagull Facts - ' + str(inter.guild.name) + ' - ' + str(inter.author) + '.')
+    elif facts == "Bidoof":
+        fact1 = 'Bidoof is a brown, rodent like Pokemon with four short legs.'
+        fact2 = 'Protruding from Bidoof’s upper jaw are large incisors, which it uses for gnawing on wood and rocks.'
+        fact3 = 'These teeth grow constantly, so this Pokémon needs to gnaw on hard substances to keep them ground down.'
+        fact4 = 'This Pokémon can be found nesting in groups near bodies of water.'
+        fact5 = 'Despite its stout body, this Pokémon can be agile and is not easily perturbed.'
+        fact6 = 'A comparison revealed that Bidoofs front teeth grow at the same rate as Rattatas.'
+        fact7 = 'Bidoof appears to be based on a beaver or a gopher. It is particularly similar to the mountain beaver, a primitive relative of beavers lacking tails.'
+        factsList = [fact1, fact2, fact3, fact4, fact5, fact6, fact7]
+        randomFact = random.choice(factsList)
+        embedVar = discord.Embed(title="Bidoof Facts!", description=randomFact, color=0x000000)
+        await inter.reply(embed=embedVar)
+        await channelPrint.send('Bidoof Facts - ' + str(inter.guild.name) + ' - ' + str(inter.author) + '.')
