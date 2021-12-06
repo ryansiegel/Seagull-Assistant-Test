6623d017abcfbafa165742f7f6b3e2fcddbfa2c7
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-06 12:37:54 -0500

    Update testbot.py

--- bot/testbot.py
+++ bot/testbot.py
@@ -9,8 +9,7 @@ bot = commands.Bot(command_prefix='!', description="This is a Helper Bot")
 inter_client = InteractionClient(bot)
 TOKEN = os.getenv("DISCORD_TOKEN")
 ALIVE = os.getenv("ALIVE_CHANNEL")
-print(ALIVE)
-ALIVE = bot.get_channel(ALIVE)
+ALIVE = bot.get_channel(int(ALIVE))
     
 
 @bot.event
