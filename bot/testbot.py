daddc07cf3d480c1d41421ab51a0114b6f3cf0da
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-06 17:24:27 -0500

    Update testbot.py

--- bot/testbot.py
+++ bot/testbot.py
@@ -1,4 +1,4 @@
-import discord,asyncio,os,re,speedtest,commands as mod,random,requests,string
+import discord,asyncio,os,re,commands as mod,random,requests,string
 from datetime import datetime, timedelta, date
 from discord.ext import commands, tasks
 from urllib import parse, request
@@ -13,38 +13,23 @@ inter_client = InteractionClient(bot)
 async def on_ready(): 
 	await asyncio.sleep(30)
 	ALIVE = bot.get_channel(int(os.getenv("ALIVE_CHANNEL")))
-	await ALIVE.send('Bot restarted - ' + str(datetime.now())[:-10])
+	await ALIVE.send('Bot restarted - ' + str(datetime.now())[:-10]) #alert when bot is started up / restarted
 	await asyncio.sleep(60)
-	if not stayAlive.is_running():
+	if not stayAlive.is_running(): #if stayAlive is not already running, then start stayAlive
 		stayAlive.start(ALIVE)	
 
 @tasks.loop(seconds=3600) #repeats every 1 hour
 async def stayAlive(ALIVE):
-	SPEED = bot.get_channel(int(os.getenv("SPEED_CHANNEL")))
-	wifi  = speedtest.Speedtest()
-	await ALIVE.send('Test Bot is alive - ' + str(datetime.now())[:-10])
-	await SPEED.send("Heroku Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nHeroku Upload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
-    
+	await ALIVE.send('Test Bot is alive - ' + str(datetime.now())[:-10]) #alert that bot is still alive
 
-#facts command
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>facts command
 @inter_client.slash_command(
 	description="Generate a random fact!",
-	options=[
-		Option(
-			"facts",
-			description="Generate a random fact!",
-			type=OptionType.STRING,
-			required=True,
-			choices=[
-				OptionChoice("bidoof", "Bidoof"),
-				OptionChoice("seagull", "Seagull"),
-			],
-		)
-	]
-)
+	options=[Option("facts",description="Generate a random fact!",type=OptionType.STRING,required=True,
+			choices=[OptionChoice("bidoof", "Bidoof"), OptionChoice("seagull", "Seagull")]])
 async def facts(inter, facts):
 	ALIVE = bot.get_channel(int(os.getenv("TEST_COMMANDS")))
-	reload(mod)
+	reload(mod) #reload commands.py
 	await mod.facts(inter, ALIVE, facts)
 
 bot.run(os.getenv("DISCORD_TOKEN"), bot=True, reconnect=True)
