e04a9e0116b3423b9042b3f57e39b4832a3468ee
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-06 13:21:45 -0500

    Update testbot.py

--- bot/testbot.py
+++ bot/testbot.py
@@ -1,4 +1,4 @@
-import discord,asyncio,os,re,speedtest,time
+import discord,asyncio,os,re,speedtest
 from datetime import datetime, timedelta, date
 from discord.ext import commands, tasks
 from urllib import parse, request
@@ -7,7 +7,6 @@ from dislash import *
 
 bot = commands.Bot(command_prefix='!', description="This is a Helper Bot")
 inter_client = InteractionClient(bot)
-TOKEN = os.getenv("DISCORD_TOKEN")
 
 @bot.event
 async def on_ready(): 
@@ -16,15 +15,14 @@ async def on_ready():
 	await ALIVE.send('Bot restarted - ' + str(datetime.now())[:-10])
 	await asyncio.sleep(60)
 	if not stayAlive.is_running():
-		stayAlive.start()	
+		stayAlive.start(ALIVE)	
 
 @tasks.loop(seconds=3600) #repeats every 1 hour
-async def stayAlive():
-	ALIVE = bot.get_channel(int(os.getenv("ALIVE_CHANNEL")))
-	speedChannel = bot.get_channel(915229249974181928)
+async def stayAlive(ALIVE):
+	SPEED = bot.get_channel(int(os.getenv("SPEED_CHANNEL")))
 	wifi  = speedtest.Speedtest()
 	await ALIVE.send('Test Bot is alive - ' + str(datetime.now())[:-10])
-	await speedChannel.send("Heroku Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nHeroku Upload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
+	await SPEED.send("Heroku Download Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.download() * 0.000001)) + " Mbps*\nHeroku Upload Speed at " + str(datetime.now())[:-10] + " is: *" + str('{0:.2f}'.format(wifi.upload() * 0.000001)) + " Mbps*")
     
 	
-bot.run(TOKEN, bot=True, reconnect=True)
+bot.run(os.getenv("DISCORD_TOKEN"), bot=True, reconnect=True)
