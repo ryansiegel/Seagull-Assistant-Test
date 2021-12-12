7d6e6d91550bea23a5f90086f1445a5e81cbb79f
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-12 11:11:07 -0500

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -37,7 +37,7 @@ async def facts(inter, channelPrint, facts):
         await channelPrint.send('Bidoof Facts - ' + str(inter.guild.name) + ' - ' + str(inter.author) + '.')
 
 async def rankList(inter, channelPrint, meta, ranks):
-    count, counting, fastmove = 1,0,None
+    count, count1, count2, fastmove, XL = 1,0,0,None,False
     matchoppt, matchrate, countoppt, countrate, chargemove = [], [], [], [], []
     if meta == "Great":
         meta = "Great League"
@@ -47,6 +47,15 @@ async def rankList(inter, channelPrint, meta, ranks):
         if count <= 5:
             if prefix.endswith('.speciesName'):
                 pokemon = str(value).upper()
+                parser1 = ijson.parse(open("bot/all-rankings-1500.json"))
+                for prefix1, event1, value1 in parser:
+                    if count < count1:
+                        if prefix.endswith('.speciesName'):
+                            if value == value1:
+                                XL = True
+                    if prefix1 == 'item':
+                        if event1 == 'end_map':    
+                            count1 += 1
             elif prefix.endswith('.matchups.item.opponent'):
                 matchoppt.append(str(value).title().replace("_"," "))
             elif prefix.endswith('.matchups.item.rating'):
@@ -56,26 +65,28 @@ async def rankList(inter, channelPrint, meta, ranks):
             elif prefix.endswith('.counters.item.rating'):
                 countrate.append(str(value))
             elif prefix.endswith('item.moveset.item'):
-                if counting == 0:
+                if count2 == 0:
                     fastmove = str(value).title().replace("_"," ")
-                    counting += 1
-                elif counting == 1 or counting == 2:
+                    count2 += 1
+                elif count2 == 1 or count2 == 2:
                     chargemove.append(str(value).title().replace("_"," ").replace(" Shadow"," (Shadow)"))
-                    counting += 1
+                    count2 += 1
             elif prefix == 'item':
                 if event == 'end_map':
-                    embedVar.add_field(name="#" + str(count) + ": " + pokemon,value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
+                    if XL == True:
+                        embedVar.add_field(name="#" + str(count) + ": " + pokemon + " (XL)",value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
+                    else:
+                        embedVar.add_field(name="#" + str(count) + ": " + pokemon,value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
                     embedVar.add_field(name="Key Wins>>",value=matchoppt[0] + " [RATING: " + matchrate[0] + "]\n" + matchoppt[1] + " [RATING: " + matchrate[1] + "]\n" + matchoppt[2] + " [RATING: " + matchrate[2] + "]\n" + matchoppt[3] + " [RATING: " + matchrate[3] +  "]\n" + matchoppt[4] + "[RATING: " + matchrate[4] + "]", inline=True)
                     embedVar.add_field(name="Key Losses>>",value=countoppt[0] + " [RATING: " + countrate[0] + "]\n" + countoppt[1] + " [RATING: " + countrate[1] + "]\n" + countoppt[2] + " [RATING: " + countrate[2] + "]\n" + countoppt[3] + " [RATING: " + countrate[3] +  "]\n" + countoppt[4] + " [RATING: " + countrate[4] + "]", inline=True)
-                    
                     matchoppt.clear()
                     matchrate.clear()
                     countoppt.clear()
                     countrate.clear()
-                    fastmove = None
-                    count += 1
-                    counting = 0
                     chargemove.clear()
+                    count += 1
+                    fastmove, XL = None,False
+                    count1, count2 = 0,0
     embedVar.set_footer(text="www.seagullsbot.com")
     await inter.reply(embed=embedVar)
     await channelPrint.send('Bidoof Facts - ' + str(inter.guild.name) + ' - ' + str(inter.author) + '.')
