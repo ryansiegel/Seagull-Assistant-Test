adbb09618c62506838bd1e8ff164d5f9b19b4a3c
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-12 14:59:57 -0500

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -87,9 +87,9 @@ async def rankList(inter, channelPrint, meta, ranks):
             elif prefix == 'item':
                 if event == 'end_map':
                     if XL == True:
-                        embedVar.add_field(name="#" + str(count) + ": " + pokemon + " (XL)",value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
+                        embedVar.add_field(name="》#" + str(count) + ": " + pokemon + " (XL)",value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
                     else:
-                        embedVar.add_field(name="#" + str(count) + ": " + pokemon,value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
+                        embedVar.add_field(name="》#" + str(count) + ": " + pokemon,value="**Suggested Moveset-** " + fastmove + " | " + chargemove[0] + " & " + chargemove[1], inline=False)
                     embedVar.add_field(name="Key Wins>>",value=matchoppt[0] + " [RATING: " + matchrate[0] + "]\n" + matchoppt[1] + " [RATING: " + matchrate[1] + "]\n" + matchoppt[2] + " [RATING: " + matchrate[2] + "]\n" + matchoppt[3] + " [RATING: " + matchrate[3] +  "]\n" + matchoppt[4] + "[RATING: " + matchrate[4] + "]", inline=True)
                     embedVar.add_field(name="Key Losses>>",value=countoppt[0] + " [RATING: " + countrate[0] + "]\n" + countoppt[1] + " [RATING: " + countrate[1] + "]\n" + countoppt[2] + " [RATING: " + countrate[2] + "]\n" + countoppt[3] + " [RATING: " + countrate[3] +  "]\n" + countoppt[4] + " [RATING: " + countrate[4] + "]", inline=True)
                     matchoppt.clear()
