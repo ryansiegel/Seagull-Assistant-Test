27aef9f4f876afda0a66e2eb86db6baef93345aa
Ryan Siegel <75775075+ryansiegel@users.noreply.github.com>
2021-12-11 19:07:13 -0500

    Update commands.py

--- bot/commands.py
+++ bot/commands.py
@@ -38,7 +38,7 @@ async def facts(inter, channelPrint, facts):
 
 async def rankList(inter, channelPrint, meta, ranks):
     count, counting, fastmove = 1,0,None
-    matchoppt, matchrate, countoppt, countrate, chargemove = []
+    matchoppt, matchrate, countoppt, countrate, chargemove = [], [], [], [], []
     if meta == "Great":
         meta = "Great League"
         parser = ijson.parse(open("/pvpoke/all/rankings-1500.json"))
